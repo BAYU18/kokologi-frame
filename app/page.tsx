1ad042e67879ad5d505934d23e935b1c59582391
BAYU18 <tanzar.rahma.bayu@gmail.com>
2025-12-24 09:52:02 +0700

    update page tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <main style={{ padding: 24 }}>
       <h1>Tes Kokologi Warna</h1>
-      <p>Buka link ini lewat Warpcast untuk memulai tes.</p>
+      <p>Buka link ini lewat Warpcast.</p>
     </main>
   );
 }
